8fd0a7cf02a4ffb9749cc93669872f11f7731f68
Joseph Redmon <pjreddie@gmail.com>
2021-02-16 19:58:07 -0800

    python3 fixes

--- tryhw5.py
+++ tryhw5.py
@@ -5,14 +5,14 @@ def softmax_model(inputs, outputs):
     return make_model(l)
 
 def neural_net(inputs, outputs):
-    print inputs
+    print(inputs)
     l = [   make_layer(inputs, 32, LOGISTIC),
             make_layer(32, outputs, SOFTMAX)]
     return make_model(l)
 
 print("loading data...")
-train = load_classification_data("mnist.train", "mnist.labels", 1)
-test  = load_classification_data("mnist.test", "mnist.labels", 1)
+train = load_classification_data(b"mnist.train", b"mnist.labels", 1)
+test  = load_classification_data(b"mnist.test",  b"mnist.labels", 1)
 print("done")
 print
 
